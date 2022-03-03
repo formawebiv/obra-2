577dee992ba9f8f0874aeb2a22259c47c63d4aa1
sformaweb <sara.formaweb@gmail.com>
2022-03-03 11:28:43 +0100

    inicio

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ require 'conexion/sesion.php';
 <body>
   <header-component></header-component>
 
-  <h2 class="text-primary my-4 mx-5">Inicio</h2>
+  <!-- <h2 class="text-primary my-4 mx-5">Inicio</h2> -->
 
   <div class="container d-flex justify-content-center mt-5">
     <div class=" bg-primary rounded-start" style="max-width: 450px; height:350px">
